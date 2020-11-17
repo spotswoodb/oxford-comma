a0265c7292c93305b1205be7e4ac7d2c81bd5692
Ben Spotswood <spotswoodb@gmail.com>
2020-11-17 15:06:19 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
-def oxford_comma(array)
+array = ["fiddleheads","okra","kohlrabi"]
 
-end
+# def oxford_comma(array)
+#     array.join(",")
+# end
+
+def oxford_comma(array)
+    array = [*array]
+    case array.size
+    when 0
+      ""
+    when 1
+      array[0].to_s
+    when 2
+      array.join(' and ')
+    else
+      array_copy = array
+      array_copy[-1] = "and #{array_copy[-1]}"
+      array_copy.join(', ')
+    end
+  end
